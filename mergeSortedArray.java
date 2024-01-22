ad6ed6fe8815c38a26759d8283c22e85d46b304c
Aras Ateşer <43793683+arasateser@users.noreply.github.com>
2024-01-22 17:55:29 +0300

    Update mergeSortedArray.java

--- mergeSortedArray.java
+++ mergeSortedArray.java
@@ -4,8 +4,9 @@ class Solution {
         int j = n - 1; //last index of nums2
         int k = m + n - 1; //last index of nums2 can get placed to
         
-        while (j >= 0) {
-            if (i >= 0 && nums1[i] > nums2[j]) {
+        while (j >= 0) {//run until all nums2 elements are matched
+                        //if number of nums2 bigger than nums1 place it before it
+            if (i >= 0 && nums1[i] > nums2[j]) { 
                 nums1[k--] = nums1[i--];
             } else {
                 nums1[k--] = nums2[j--];
