732bc5e3d19061c093c1c9c3f284fd5b63268bf3
Aras Ateşer <43793683+arasateser@users.noreply.github.com>
2024-02-16 14:33:11 +0300

    Update longestPalindrome.java

--- longestPalindrome.java
+++ longestPalindrome.java
@@ -1,28 +1,28 @@
 class Solution {
     public int longestPalindrome(String s) {
         Map<Character, Integer> charCount = new HashMap<>();
-        int polCount = 0; //longest polindrome
-        int largestOdd = 0; //largest odd number of characters
+        int polCount = 0; // longest polindrome
+        // int largestOdd = 0; //largest odd number of characters
+        boolean hasOdd = false;
 
-        for (char c : s.toCharArray()) { //counting every character
+        for (char c : s.toCharArray()) { // counting every character
             charCount.put(c, charCount.getOrDefault(c, 0) + 1);
         }
 
-        for (Map.Entry<Character, Integer> entry : charCount.entrySet()) { //iterate through each character
+        for (Map.Entry<Character, Integer> entry : charCount.entrySet()) { // iterate through each character
 
-            if (entry.getValue() % 2 == 0) { //if even include the number of characters to the polindrome count
+            if (entry.getValue() % 2 == 0) { // if even include the number of characters to the polindrome count
                 polCount += entry.getValue();
-            } else{ //find the largest odd number of characters
-                if (largestOdd < entry.getValue()) {
-                    largestOdd = entry.getValue();
+            } else {
+                if (entry.getValue() > 1) {
+                    polCount = polCount + entry.getValue() - 1;
                 }
-            }          
-
+                if (hasOdd == false) {
+                    polCount += 1;
+                    hasOdd = true;
+                }
+            }
         }
-
-        //if all the characters are even add '0'
-        //if not add the largest odd number of characters
-        polCount += largestOdd; 
         return polCount;
     }
 }
