78de8767a43375e052cb7b60ab2e641040c925a5
Aras Ateşer <43793683+arasateser@users.noreply.github.com>
2024-02-23 16:56:20 +0300

    Update squaresOfASortedArray.java

--- squaresOfASortedArray.java
+++ squaresOfASortedArray.java
@@ -6,7 +6,11 @@ class Solution {
         int[] array = new int[nums.length];
 
         for (int i = 0; i < nums.length; i++) {
-            if (Math.pow(nums[head], 2) > Math.pow(nums[tail], 2)) {
+            if (Math.pow(nums[head], 2) > Math.pow(nums[tail], 2)) { // whatever is the bigger place it end of the array
+                                                                     // because we started matching the values from the
+                                                                     // head and the tail which are the biggest and
+                                                                     // smallest numbers in the array and we keep
+                                                                     // matching until traversing every int
                 array[index] = Math.abs(nums[head] * nums[head]);
                 head++;
                 index--;
