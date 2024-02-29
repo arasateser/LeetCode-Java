6510392812f4faaff1196c8d9522d439dc1e8410
Aras Ateşer <43793683+arasateser@users.noreply.github.com>
2024-02-29 13:30:02 +0300

    Create invertBinaryTree.java

---
+++ invertBinaryTree.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+public class Solution {
+    public TreeNode invertTree(TreeNode root) {
+
+        if (root == null) {
+            return null;
+        }
+
+        final Deque<TreeNode> stack = new LinkedList<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            TreeNode currentNode = stack.pop();
+            TreeNode left = currentNode.left;
+            currentNode.left = currentNode.right;
+            currentNode.right = left;
+
+            if (currentNode.left != null) {
+                stack.push(currentNode.left);
+            }
+            if (currentNode.right != null) {
+                stack.push(currentNode.right);
+            }
+        }
+        return root;
+    }
+}
